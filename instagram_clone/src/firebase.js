0772dce4d17f984fc7b586fb622b67c39d23429f
Kareem Krayem <50558667+Kareem-Krayem@users.noreply.github.com>
2020-08-22 03:13:04 +0300

    Update firebase.js

--- instagram_clone/src/firebase.js
+++ instagram_clone/src/firebase.js
@@ -1,15 +1,9 @@
 import firebase from 'firebase';
 
-const config = {
-    apiKey: "AIzaSyDaNVQ-aE94lJatdnBHmu4MpF6YPNHcvac",
-    authDomain: "kareem-instagram-clone.firebaseapp.com",
-    databaseURL: "https://kareem-instagram-clone.firebaseio.com",
-    projectId: "kareem-instagram-clone",
-    storageBucket: "kareem-instagram-clone.appspot.com",
-    messagingSenderId: "967033896911",
-    appId: "1:967033896911:web:d88b0e3e5a19f511601cba",
-    measurementId: "G-Q0YRN3H8C9"
-};
+const firebaseApp = firebase.initializeApp({
+//here are the configurations that you need to put from firebase
+});
+
 const firebaseApp = firebase.initializeApp(config);
 
 const db = firebaseApp.firestore();
